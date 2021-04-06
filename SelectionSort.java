70d32118a3abf4a0c118a365326dbd0a52163676
hyperminji <hyperminji@gmail.com>
2021-04-06 23:17:43 +0900

    SelectionSort

---
+++ SelectionSort.java
@@ -0,0 +1,25 @@
+public class SelecrionSort{
+    public static void swapElements(int[] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j]= temp;
+    }
+    //find min value(include start),lastpoint of list
+    public static int indexLowest(int[] array, int start){
+        int lowIndex = start;
+        for (int i = start; i< array.length; i++){
+            if (array[i] < array[lowIndex]){
+                lowIndex = i;
+            }
+        }
+        return lowIndex;
+    }
+    //sort values ,  selecrionsort
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int j = indexLowest(array, i);
+            swapElements(array, i ,j);
+        }
+    }
+
+}
